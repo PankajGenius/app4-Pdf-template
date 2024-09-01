a4da7996387171c63b5d2b3fd9a56121311635c6
surya2402 <suryagupta654321@gmail.com>
2024-09-01 20:12:42 +0530

    Added footer

--- main.py
+++ main.py
@@ -3,21 +3,32 @@ import pandas as pd
 
 # P for Portrait and #L can landscape
 pdf = FPDF(orientation="P", unit="mm", format="A4")
-
+pdf.set_auto_page_break(auto=False, margin=0)
 
 df = pd.read_csv("topics.csv")
 
 for index, row in df.iterrows():
 
+    # set the header
     pdf.add_page()
-
     pdf.set_font(family="Helvetica", style="B", size=12)
     pdf.set_text_color(254, 0, 0)
     pdf.cell(w=0, h=12, txt=row["Topic"],  align="L", ln=1)
     # w for width , h for height, ln = line break
     pdf.line(10,21, 200, 21)
 
+    # set the footer
+    pdf.ln(269)
+    pdf.set_font(family="Helvetica", style="I", size=8)
+    pdf.set_text_color(180, 120, 160)
+    pdf.cell(w=0, txt=row["Topic"], align="R")
+
     for i in range(row["Pages"]-1):
         pdf.add_page()
+        # set the footer
+        pdf.ln(281)
+        pdf.set_font(family="Helvetica", style="I", size=8)
+        pdf.set_text_color(180, 120, 160)
+        pdf.cell(w=0, txt=row["Topic"], align="R")
 
 pdf.output("Output.pdf")
