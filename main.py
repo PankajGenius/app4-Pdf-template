54f0820417f9c03e4e834dc0094df8d61f384c7c
surya2402 <suryagupta654321@gmail.com>
2024-09-01 19:40:13 +0530

    added multiple pages

--- main.py
+++ main.py
@@ -12,8 +12,12 @@ for index, row in df.iterrows():
     pdf.add_page()
 
     pdf.set_font(family="Helvetica", style="B", size=12)
+    pdf.set_text_color(254, 0, 0)
     pdf.cell(w=0, h=12, txt=row["Topic"],  align="L", ln=1)
     # w for width , h for height, ln = line break
     pdf.line(10,21, 200, 21)
+
+    for i in range(row["Pages"]-1):
+        pdf.add_page()
 
 pdf.output("Output.pdf")
